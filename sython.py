18b57b316da421f03f23953f56336054f4d6cb43
isRAMY <134105669+isRAMY@users.noreply.github.com>
2023-07-02 17:33:40 +0300

    Add files via upload

---
+++ sython.py
@@ -0,0 +1,154 @@
+import telethon
+from telethon import events
+from config import *
+import os
+import logging
+import asyncio
+import time
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import FloodWaitError
+from telethon import TelegramClient, events
+from collections import deque
+from telethon import functions
+from telethon.errors.rpcerrorlist import (
+    UserAlreadyParticipantError,
+    UserNotMutualContactError,
+    UserPrivacyRestrictedError,
+)
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.types import InputPeerUser
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions
+from hijri_converter import Gregorian
+from telethon.tl.functions.channels import LeaveChannelRequest
+import base64
+import datetime
+from payment import *
+from help import *
+from checktele import *
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+import requests
+# -
+
+sython.start()
+c = requests.session()
+bot_username = '@zmmbot'
+bot_usernamee = '@A_MAN9300BOT'
+
+y = datetime.datetime.now().year
+m = datetime.datetime.now().month
+dayy = datetime.datetime.now().day
+day = datetime.datetime.now().strftime("%A")
+m9zpi = f"{y}-{m}-{dayy}"
+sec = time.time()
+
+LOGS = logging.getLogger(__name__)
+
+DEVS = [
+   1041483862,
+]
+DEL_TIME_OUT = 10
+normzltext = "1234567890"
+namerzfont = normzltext
+name = "Profile Photos"
+time_name = ["off"]
+time_bio = ["off"]
+
+
+async def join_channel():
+    try:
+        await sython(JoinChannelRequest("i_l_n"))
+    except BaseException:
+        pass
+
+
+
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.الاوامر"))
+async def _(event):
+    await event.edit(commands)
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.فحص"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit("جارٍ...")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(f'''ᴄʜᴇᴄᴋᴇʀ ɪs ʀᴜɴɪɴɢ ♕\n𝙿ᴏɴɢ ↬ `{ms}`\nᴅᴀᴛᴇ ↬ `{m9zpi}`\nᴄʜᴀᴛ ɪᴅ ↬ `{event.sender_id}`\nᴅᴇᴠ ᴄʜᴇᴄᴋᴇʀ ↬ [- ɴᴏ ɴᴀᴍᴇ <\>](https://t.me/w_y_o)''')
+
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.م1"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit(sec1)
+
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.م2"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit(sec2)
+    
+ownerhson_id = 5774423143
+@sython.on(events.NewMessage(outgoing=False, pattern='/Dev'))
+async def OwnerStart(event):
+    sender = await event.get_sender()
+    if sender.id == ownerhson_id :
+        order = await event.reply('اهلا مطوري  [𓆩 𝑊 𓆪 •](https://t.me/Z_b_Z)')
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
+async def update(event):
+    await event.edit("• جارِ اعادة تشغيل السورس ..\n• انتضر 1-2 دقيقة  .")
+    await sython.disconnect()
+    await sython.send_message(event.chat.id, "`اكتملت اعادة تشغيل السورس`")
+
+
+async def join_channel():
+    try:
+        await sython(JoinChannelRequest("@i_l_n"))
+    except BaseException:
+        pass
+ 
+ 
+GCAST_BLACKLIST = [
+    -1001884452589,
+    -1001884452589,
+]
+
+DEVS = [
+    5774423143,
+]
+
+def calc(num1, num2, fun):
+    if fun == "+":
+        return num1 + num2
+    elif fun == "-":
+        return num1 - num2
+    elif fun == "*":
+        return num1 * num2
+    elif fun == "X":
+        return num1 * num2
+    elif fun == "x":
+        return num1 * num2
+    elif fun == "/":
+        return num1 / num2
+    elif fun == "÷":
+        return num1 / num2
+    else:
+        return "خطأ"
+
+
+@sython.on(events.NewMessage(outgoing=True, pattern=".مؤقت (.*)"))
+async def spammer(event):
+    reply = await event.get_reply_message()
+    input_str = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    sleeptimet = sleeptimem = float(input_str[0])
+    cat = input_str[1:]
+    await event.delete()
+    await spam_function(event, reply, cat, sleeptimem, sleeptimet, DelaySpam=True)
+
+print("- No Name <\>  Userbot Running ..")
+sython.run_until_disconnected()
